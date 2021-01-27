72b21445984a8626d24982f25bd445aab8ccf8d8
HimanshuDubara <himanshudubara2000@gmial.com>
2021-01-27 14:31:17 +0530

    Hello

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/himanshu/nav_sys/devel:$CMAKE_PREFIX_PATH"
+export PWD='/home/himanshu/nav_sys/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/himanshu/nav_sys/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/himanshu/nav_sys/src:$ROS_PACKAGE_PATH"
